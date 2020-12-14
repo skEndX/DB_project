81617821855b71f13e132f14e35e4b50118011c4
skEndX <71227122+skEndX@users.noreply.github.com>
2020-12-15 02:13:44 +0900

    Update pet_start.py

--- pet_start.py
+++ pet_start.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-#from tkinter import messagebox as msg
 import sqlite3
 
 app = Flask(__name__)
@@ -259,19 +258,101 @@ def reservation_check(phoneNumber):
     db = sqlite3.connect("pet.db")
     db.row_factory = sqlite3.Row
 
-    sql="select * from reservation where m_num='"+phoneNumber+"'"
+    sql="SELECT i_name,i.i_num, p_name, r_day, r_time \
+        FROM reservation r, instructor i, pet p \
+        WHERE r.i_num=i.i_num and p.p_num=r.p_num and r.m_num='"+phoneNumber+ \
+        "' order by p_name, r_day"
+
     items = db.execute(sql).fetchall()
 
     db.close()
     return render_template('reservation_check.html',phoneNumber=phoneNumber, items=items)
+
+@app.route('/pet/login/login_success/mypage/pet_check/<phoneNumber>/', methods=['GET','POST'])
+def pet_check(phoneNumber):
+    db = sqlite3.connect("pet.db")
+    db.row_factory = sqlite3.Row
+
+    sql1="select * from pet where m_num='"+phoneNumber+"'"
+    p_item=db.execute(sql1).fetchall()
+        
+    db.close()
+    return render_template('pet_check.html',phoneNumber=phoneNumber,p_item=p_item)
+
+@app.route('/pet/login/login_success/mypage/pet_check/pet_edit/<phoneNumber>/<p_num>', methods=['GET','POST'])
+def pet_edit(phoneNumber,p_num):
+    if request.method=='POST':
+        db = sqlite3.connect("pet.db")
+        db.row_factory = sqlite3.Row
+        
+        name=request.form['p_name']
+        num=request.form['p_number']
+        weight=request.form['p_weight']
+        
+        val=(name, weight)
+        sql="update pet set p_name=?,p_weight=? where m_num='"+phoneNumber+"' and p_num='"+num+"'"
+
+        db.execute(sql, val)
+
+        db.commit()
+        db.close()
+        return redirect(url_for('pet_check',phoneNumber=phoneNumber))
+    else:
+        db = sqlite3.connect("pet.db")
+        db.row_factory = sqlite3.Row
+
+        sql1="select * from pet where m_num='"+phoneNumber+"' and p_num='"+p_num+"'"
+        p_item=db.execute(sql1).fetchone()
+        
+        db.close()
+        return render_template('pet_edit.html',phoneNumber=phoneNumber,p_item=p_item)
 
 @app.route('/pet/login/login_success/mypage/pet_diary_check/<phoneNumber>/')
 def pet_diary_check(phoneNumber):
     db = sqlite3.connect("pet.db")
     db.row_factory = sqlite3.Row
 
-    sql="select * from daily_log where m_num='"+phoneNumber+"'"
+    sql="select p.p_name,log_day,sleep,diet \
+         from daily_log d, pet p\
+         where d.m_num='"+phoneNumber+"' and p.p_num=d.p_num\
+         order by p.p_name, log_day"
+
     items = db.execute(sql).fetchall()
 
     db.close()
     return render_template('pet_diary_check.html',phoneNumber=phoneNumber, items=items)
+
+@app.route('/pet/login/login_success/mypage/my_info/<phoneNumber>/', methods=['GET','POST'])
+def my_info(phoneNumber):
+    if request.method=='POST':
+        db = sqlite3.connect("pet.db")
+        db.row_factory = sqlite3.Row
+        
+        name=request.form['mem_name']
+        num=request.form['mem_number']
+        addr=request.form['mem_addr']
+        sex=request.form['mem_sex']
+        
+        if num=='':
+            return redirect(url_for('login_fail'))
+        
+        val=(num, name, addr, sex)
+        sql="update member set m_num=?,m_name=?, m_addr=?, m_sex=? where m_num='"+phoneNumber+"'"
+
+        db.execute(sql, val)
+
+        db.commit()
+        db.close()
+        return redirect(url_for('mypage',phoneNumber=num))
+    else:
+        db = sqlite3.connect("pet.db")
+        db.row_factory = sqlite3.Row
+
+        sql="select * from member where m_num='"+phoneNumber+"'"
+        item=db.execute(sql).fetchone()
+        db.close()
+        return render_template('my_info.html',phoneNumber=phoneNumber,item=item)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000)
